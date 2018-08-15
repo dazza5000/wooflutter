75362dbb25960f45bf155890f9eb612fbb529715
Darran Kelinske <darran.kelinske@gmail.com>
2018-08-14 20:57:57 -0500

    remove refinement buttons

--- lib/pages/products_list_page.dart
+++ lib/pages/products_list_page.dart
@@ -30,62 +30,8 @@ class ProductsListPage extends StatelessWidget {
       child: ListView.builder(
         itemCount: 5,
         itemBuilder: (context, index) {
-          if (index == 0) {
-            return _buildFilterWidgets(screenSize);
-          } else if (index == 4) {
-            return SizedBox(
-              height: 12.0,
-            );
-          } else {
             return _dummyProductsList()[index];
-          }
         },
-      ),
-    );
-  }
-
-  _buildFilterWidgets(Size screenSize) {
-    return Container(
-      margin: const EdgeInsets.all(12.0),
-      width: screenSize.width,
-      child: Card(
-        elevation: 4.0,
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 12.0),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              _buildFilterButton("SORT"),
-              Container(
-                color: Colors.black,
-                width: 2.0,
-                height: 24.0,
-              ),
-              _buildFilterButton("REFINE"),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  _buildFilterButton(String title) {
-    return InkWell(
-      onTap: () {
-        print(title);
-      },
-      child: Row(
-        children: <Widget>[
-          Icon(
-            Icons.arrow_drop_down,
-            color: Colors.black,
-          ),
-          SizedBox(
-            width: 2.0,
-          ),
-          Text(title),
-        ],
       ),
     );
   }
